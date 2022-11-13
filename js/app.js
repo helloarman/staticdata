22313aaff861af599c883f475382f6465cdaa95a
Arman Rahman <armanrahman0@gmail.com>
2022-11-13 20:02:38 +0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,68 +1,28 @@
-var enterPress = document.getElementById("body");
-enterPress.onkeyup = function(runOnKeyPress){
-    if(runOnKeyPress.keyCode == 13){
-      output();
-    }
-}
-
-function inputProcess(){
-    var json_file_name = document.getElementById('json_file_name').value;
-    var text = document.getElementById('text').value;
-
-    var out = text && text.match(
-    /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-            .map(s => s.toLowerCase())
-            .join('_');
-    
-    return {out, text, json_file_name};
-}
-
-function output(){
-    var processedText = inputProcess();
-
-    // Restore Copied File For JSON
-    var json_copy = document.getElementById('json_copy_btn');
-    json_copy.classList.remove("button-copied");
-    document.getElementById('json_copy_btn').innerHTML = `Copy JSON <i class="fa-regular fa-copy"></i>`;
-    // Restore Copied File For BLADE
-    var json_copy = document.getElementById('blade_copy_btn');
-    json_copy.classList.remove("button-copied");
-    document.getElementById('blade_copy_btn').innerHTML = `Copy Blade <i class="fa-regular fa-copy"></i>`;
-
-    var json_output_text = `"${processedText.out}" : "${processedText.text}"`;
-    var blade_output_text = `{{ ___('${processedText.json_file_name}.${processedText.out}') }}`;
-
-    document.getElementById('json_text_for_output').innerHTML = json_output_text;
-    document.getElementById('blade_text_for_output').innerHTML = blade_output_text;
-}
+let add = document.getElementById('add');
+let output_list = document.getElementById('output_list');
+let attribute_name = document.getElementById('attribute_name');
 
 
-function copyjson(){
-    var processedText = inputProcess();
+add.addEventListener('click',function(){
+    var attribute_name_load = attribute_name.value;
 
-    var json_output_text = `"${processedText.out}" : "${processedText.text}"`
+    var out = attribute_name_load && attribute_name_load.match(
+        /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+                .map(s => s.toLowerCase())
+                .join('_');
+    var attribute = document.createElement('span');
+    attribute.classList.add('paragraph-styling');
+    attribute.innerText =`'${out}' => 'xyz',`;
 
-    var copyText = json_output_text;
+    var attribute2 = document.createElement('span');
+    attribute2.classList.add('paragraph-styling');
+    attribute2.innerText =`{{ @$topCourse['${out}'] }}`;
 
-    // After Copy 
-    var json_copy = document.getElementById('json_copy_btn');
-    json_copy.classList.add("button-copied");
-    document.getElementById('json_copy_btn').innerHTML = "JSON Copied!"
-    
-    navigator.clipboard.writeText(copyText);
-}
+    var attribute3 = document.createElement('span');
+    attribute3.classList.add('paragraph-styling');
+    attribute3.innerText =`{{ $topCourse->${out} }}`;
 
-function copyblade(){
-    var processedText = inputProcess();
-
-    var blade_output_text = `{{ ___('${processedText.json_file_name}.${processedText.out}') }}`
-
-    var copyText = blade_output_text;
-
-    // After Copy 
-    var json_copy = document.getElementById('blade_copy_btn');
-    json_copy.classList.add("button-copied");
-    document.getElementById('blade_copy_btn').innerHTML = "Blade Copied!"
-    
-    navigator.clipboard.writeText(copyText);
-}
+    output_list.appendChild(attribute);
+    output_list_2.appendChild(attribute2);
+    output_list_3.appendChild(attribute3);
+})
